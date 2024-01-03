c787a590b354c4ab4162a120b95df58d30544cb2
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2024-01-03 16:56:46 +0700

    feat: add custome style

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,18 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+        dark: "rgb(var(--tw-clr-dark) / <alpha-value>)",
+        light: "rgb(var(--tw-clr-light) / <alpha-value>)",
+        secondary: "rgb(var(--tw-clr-secondary) / <alpha-value>)",
+        accent: {
+          peach: "rgb(var(--tw-clr-accent-peach) / <alpha-value>)",
+          grape: "rgb(var(--tw-clr-accent-grape) / <alpha-value>)",
+        },
+      },
+      boxShadow: {
+        accent: "0 0 10px 2px rgb(var(--tw-clr-accent-peach) / 0.5)",
+      },
       fontFamily: {
         sans: ["Inter", ...fontFamily.sans],
       },
