d18ddf68be1d6b9da0e3dd8327cf791528b1cc34
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2024-01-03 18:59:14 +0700

    fix: navbar bg color

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -4,7 +4,10 @@ import clsx from "clsx";
 
 const Navbar = ({ className, ...rest }) => {
   return (
-    <header className={clsx("sticky top-0 z-[1]", className)} {...rest}>
+    <header
+      className={clsx("sticky top-0 z-[1] bg-zinc-800", className)}
+      {...rest}
+    >
       <Container className="flex items-center justify-between py-5">
         <h1>SIG Test</h1>
       </Container>
